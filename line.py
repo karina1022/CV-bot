f4692cdb426280e46767d3425d86f5a9e1c913a5
karina1022 <karinajen1022@gmail.com>
2021-07-25 04:07:42 +0100

    args

--- line.py
+++ line.py
@@ -1,7 +1,7 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-CHANNEL_ACCESS_TOKEN = ""
+CHANNEL_ACCESS_TOKEN = "OrCb+t6DK58FLZyYkx24iM5Zh7ngO5wgc3vwB3BulAW7mvnBH1Skb9n8nC/P4lp00uxRiUMtTH1USGGzyexg1pWe7Wh35lvwft5c/r1zKSRoMncSGEH+MSafIf7ebI4zru/q1VhZUsmlgFvk5jma8gdB04t89/1O/w1cDnyilFU="
 
 def line_bot(value):
     line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
